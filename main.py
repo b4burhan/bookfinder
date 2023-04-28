91de1771a795461832e6afaddecdeb79a5fea634
Amaan Khan <a4amaan@yahoo.com>
2023-04-28 23:01:01 +0500

    sdsd

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import uvicorn as uvicorn
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from fastapi.testclient import TestClient
 from pymongo import MongoClient
 
 app = FastAPI()
+client = TestClient(app)
 
 # MongoDB configuration
 client = MongoClient('mongodb://localhost:27017/')
@@ -53,6 +55,12 @@ async def get_all_books():
     return jsonable_encoder(books)
     # Return the results
     return jsonify({'books': books})
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
 
 
 if __name__ == '__main__':
